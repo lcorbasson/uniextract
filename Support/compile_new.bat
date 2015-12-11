0d531eedc056faef1b4f4273bc1074a39784ffce
Loïc CORBASSON <loic.corbasson@gmail.com>
2015-12-11 17:18:16 +0100

    Cleanup stale files

--- Support/compile_new.bat
+++
@@ -1,2 +0,0 @@
-"Y:\windows\apps\CompileAU3.exe" /in UniExtract.au3 /out ..\bin\UniExtract.exe /comp 4 /icon Icons\uniextract_exe.ico /nodecompile /prod
-rem /AutoIT3Dir "C:\Program Files\AutoIt3\Aut2Exe\Aut2Exe.exe"
--- Support/generate_noinst.bat
+++
@@ -1,3 +0,0 @@
-del ..\uniextract08_noinst.exe
-winrar.exe a -iiconIcons\uniextract_files.ico -m5 -r -s -sfx ..\uniextract08_noinst.exe ..\todo.txt ..\bin ..\docs ..\Support\UniExtract.* ..\Support\compile* ..\Support\generate* ..\Support\Icons
-winrar.exe c -zgenerate_comments.txt ..\uniextract08_noinst.exe
